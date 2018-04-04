4cd835a372971ee09c26f97f6631859f4044cd43
Alex <github email address>
2018-04-04 18:40:24 -0400

    index page, item show page, shop show page with routes

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,6 @@
+class ShopsController < ApplicationController
+  def show
+      @shop = Shop.find_by(id: params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  #this provides us with all of the routes that devise gives us for users
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'genericpage#index', as: 'genericpage'
 
+  root 'genericpage#index', as: 'genericpage'
+  resources :items, only: [:show]
+  resources :shops, only: [:show]
+
+  #
 
 end
