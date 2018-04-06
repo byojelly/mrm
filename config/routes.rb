bcb25e3fabe02a629535a533969cf1e676416b56
Alex <github email address>
2018-04-06 14:18:42 -0400

    routes had to be modified for call back  devise_for :users, :controllers => { :omniauth_callbacks => callbacks }   also had to run rake routes to understand than user_github_omniauth_authorize_path should be used when setting up link_to in views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   #github omniauth Add omniauth callbacks to config/routes.rb that will route to callbacks controller which will be created
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'genericpage#home', as: 'genericpage'
