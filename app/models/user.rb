becbe42cfa3d95222de8a7491a0aa6e6c7268e87
Alex <github email address>
2018-04-06 13:55:35 -0400

    make user model omniauuthable and add columns to user table, add method for interpreting omniauth info in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  #adding for facebook omniauth
-  #devise :omniauthable, omniauth_providers: %i[facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  #omniauthable added above
 
   #validations  great thing about deivse, is that the below validations as well as more come standard with devise
   #validates :email, presence: true
@@ -22,18 +21,16 @@ class User < ApplicationRecord
     #should also setup code to setup an admin as well
 
   end
-    #This code will take the data that Google returns and persist it to the database. If the user does not exist, a new one will be created, otherwise, the existing user will be updated.
-#  def self.from_omniauth(auth)
-#binding.pry
-#    where(provider: auth.provider, uid: #auth.uid).first_or_initialize.tap do |user|
-#      user.provider = auth.provider
-#      user.uid = auth.uid
-#      user.name = auth.info.name
-#      user.oauth_token = auth.credentials.token
-#      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-#      user.save!
-#    end
-#  end
+    #This code will take the data that Github(or other omniauth) returns and persist it to the database. If the user does not exist, a new one will be created, otherwise, the existing user will be updated.
+    def self.from_omniauth(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+          user.provider = auth.provider
+          user.uid = auth.uid
+          user.email = auth.info.email
+          user.password = Devise.friendly_token[0,20]
+        end
+      end
+
 
 
 end
--- db/migrate/20180405201717_add_omni_auth_facebook_info_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddOmniAuthFacebookInfoToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :image, :string
-    add_column :users, :uid, :string
-  end
-end
