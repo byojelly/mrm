81b7dcdcfb9399b1879bf97820fa07a6376d6231
Alex <github email address>
2018-04-06 12:34:22 -0400

    2ND ATTEMPT AT GOOGLE LOG IN, 1st attempt did not coordinate with devise therfore the commits were rolled back to this point.  This commit makes the following changes in attempt to restore to a fresh point to tackle this opmniath. remove gems = dot env omni auth facebook thin, .env root file, commented out sessions file, delete initalize/omniauth.rb file, .gitignore file remove .env extension, commented out facebook omniauth route, user.rb file commented out self.from_omniauth method,

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  #adding for facebook omniauth
+  #devise :omniauthable, omniauth_providers: %i[facebook]
 
   #validations  great thing about deivse, is that the below validations as well as more come standard with devise
   #validates :email, presence: true
@@ -18,6 +20,20 @@ class User < ApplicationRecord
   def set_normal_role
     #code to set user role to normal
     #should also setup code to setup an admin as well
-    
+
   end
+    #This code will take the data that Google returns and persist it to the database. If the user does not exist, a new one will be created, otherwise, the existing user will be updated.
+#  def self.from_omniauth(auth)
+#binding.pry
+#    where(provider: auth.provider, uid: #auth.uid).first_or_initialize.tap do |user|
+#      user.provider = auth.provider
+#      user.uid = auth.uid
+#      user.name = auth.info.name
+#      user.oauth_token = auth.credentials.token
+#      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+#      user.save!
+#    end
+#  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405201717) do
+ActiveRecord::Schema.define(version: 20180406135945) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -71,9 +71,12 @@ ActiveRecord::Schema.define(version: 20180405201717) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "image"
+    t.string "user"
+    t.string "provider"
     t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
