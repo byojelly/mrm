f19ca2edcecd28c160f388d3535a06b2dc74cbec
Alex <github email address>
2018-04-08 18:16:44 -0400

    working on amazon solution

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -5,7 +5,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
     sign_in_and_redirect @user
   end
   def amazon
-    binding.pry
+  #  binding.pry
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
   end
