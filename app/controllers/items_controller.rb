7a39c3aa21cf6d73aa5a9e725f43b177737fb6ec
Alex <github email address>
2018-04-09 23:09:46 -0400

    ISSUE - running into trouble implementing the new item form using nested resources

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,14 @@
 class ItemsController < ApplicationController
   before_action :set_item, only:[:show, :edit, :update, :item]
+  before_action :set_shop, only:[:new]
 def show
   #binding.pry
 end
 def new
     @item=Item.new
-    if params[:shop_id]
-      @shop = Shop.find_by(id: params[:shop_id])
-    end
 end
 def create
-#binding.pry
+binding.pry
   @item = Item.new(item_params)
   @item.save
   redirect_to item_path(@item)
@@ -30,6 +28,10 @@ def set_item
   #binding.pry
   @item = Item.find_by(id: params[:id])
 end
+def set_shop
+  @shop = Shop.find_by(id: params[:shop_id])
+end
+
 def item_params
   params.require(:item).permit(:name, :year, :condition, :detail, :itemtype, :available, :quantity, :shop_id)
 end
