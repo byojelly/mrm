9c233acc2cf6cc7c4732380d9c6022e11e27e8cf
Alex <github email address>
2018-04-15 20:35:11 -0400

    about to generate users controller to allow access for users to only edit their own account contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,34 @@
 class ContactsController < ApplicationController
+      before_action :set_contact, only:[:show, :edit, :update]
 
+    def show
+binding.pry
+    end
+    def edit
+      require_logged_in
+    end
+    def update
+  #  binding.pry
+        if @contact.update(contact_params)
+  #  binding.pry
+            redirect_to contact_path(@contact)
+        else
+            render 'edit'
+        end
+    end
+
+
+private
+    def require_logged_in
+        if current_user.id != params[:id].to_i
+              redirect_to contact_show_path
+        end
+    end
+    def set_contact
+      #binding.pry
+      @contact = Contact.find_by(id: params[:id])
+    end
+    def contact_params
+      params.require(:contact).permit(:name, :address, :city, :state, :zip, :phone)
+    end
 end
